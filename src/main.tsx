8526f9f839953d056325034cd9f707b9a896c798
Davide Carpini <carpini.davide@gmail.com>
2025-01-13 18:33:17 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <Provider>
-            <App />
-        </Provider>
+        <HashRouter basename="/score-system-showcase">
+            <Provider>
+                <App />
+            </Provider>
+        </HashRouter>
     </StrictMode>
 );
