1824ccd6f3af3d27f953d139df54afafaac793c3
Davide Carpini <carpini.davide@gmail.com>
2025-01-13 19:07:01 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     base: "/score-system-showcase/",
+    build: {
+        outDir: "docs", // <--- set the build output to docs
+    },
     plugins: [react()],
 });
